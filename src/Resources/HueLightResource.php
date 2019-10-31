226ad6b68db1eddc05a8992342b275d3f3baa27f
Allen Taylor <allen.taylor@check24.de>
2019-10-31 23:48:22 +0100

    removed doc due to return type not yet clear

--- src/Resources/HueLightResource.php
+++ src/Resources/HueLightResource.php
@@ -4,9 +4,7 @@ namespace Philips\Hue\Resources;
 
 class HueLightResource extends BaseHueResource
 {
-    /**
-     * @return mixed
-     */
+
     public function isLightOn()
     {
         return $this->state->on;
